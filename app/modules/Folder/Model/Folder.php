d1abe5c8baa128932a661767883febc3247d4b31
Adrien SALA <a.sala@xtraball.com>
2014-05-21 18:49:53 +0200

    Update the Folder model class

--- app/modules/Folder/Model/Folder.php
+++ app/modules/Folder/Model/Folder.php
@@ -1,10 +1,34 @@
 <?php
 class Folder_Model_Folder extends Core_Model_Default {
 
+    protected $_root_category;
+
     public function __construct($params = array()) {
         parent::__construct($params);
         $this->_db_table = 'Folder_Model_Db_Table_Folder';
         return $this;
+    }
+
+    public function delete() {
+
+        if(!$this->getId()) {
+            return $this;
+        }
+
+        $this->getRootCategory()->delete();
+
+        return parent::delete();
+    }
+
+    public function getRootCategory() {
+
+        if(!$this->_root_category) {
+            $this->_root_category = new Folder_Model_Category();
+            $this->_root_category->find($this->getRootCategoryId());
+        }
+
+        return $this->_root_category;
+
     }
 
 }
