54f66a72a012f5582759d4580833f42cb2e0bc07
Adrien SALA <a.sala@xtraball.com>
2014-06-23 19:46:24 +0200

    Update the translation on the Log In page

--- app/modules/Front/controllers/IndexController.php
+++ app/modules/Front/controllers/IndexController.php
@@ -10,6 +10,9 @@ class IndexController extends Core_Controller_Default {
             if($this->getRequest()->isInstalling()) $layout_id = 'installer_installation_index';
             else if(!$this->getSession()->isLoggedIn()) $layout_id = 'admin_account_login';
             else $layout_id = 'application_customization_design_style_edit';
+
+            $module = substr($layout_id, 0, stripos($layout_id, '_'));
+            Core_Model_Translator::addModule($module);
         }
 
         $this->loadPartials($layout_id);
