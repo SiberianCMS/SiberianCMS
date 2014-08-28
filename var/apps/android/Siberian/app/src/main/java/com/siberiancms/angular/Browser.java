8f9c3b5686bbdec4141123958449720d5652bcbd
Adrien SALA <a.sala@xtraball.com>
2014-08-28 18:27:11 +0200

    Updating the Android sources

---
+++ var/apps/android/Siberian/app/src/main/java/com/siberiancms/angular/Browser.java
@@ -0,0 +1,143 @@
+package com.siberiancms.angular;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.WebChromeClient;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class Browser extends Activity {
+	private ImageButton bBack;
+	private ImageButton bForward;
+	private ImageButton bReload;
+	private Button bDone;
+    private WebView webView;
+    private boolean mIsLoadFinish = false;
+    
+    protected void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+		setContentView(R.layout.browser);
+		
+		webView = (WebView) findViewById(R.id.webView);
+		webView.getSettings().setSupportZoom(true);
+		webView.getSettings().setBuiltInZoomControls(true);
+        webView.getSettings().setDisplayZoomControls(false);
+
+		webView.getSettings().setUseWideViewPort(true);
+		webView.getSettings().setJavaScriptEnabled(true);
+		webView.getSettings().setUseWideViewPort(true);
+		webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+		webView.setScrollBarStyle(WebView.SCROLLBARS_INSIDE_OVERLAY);
+		
+		bBack = (ImageButton) findViewById(R.id.bBack);
+		bForward = (ImageButton) findViewById(R.id.bForward);
+		bReload = (ImageButton) findViewById(R.id.bReload);
+        bDone = (Button) findViewById(R.id.bDone);
+		
+    	String url = (String) getIntent().getSerializableExtra("url");
+    	
+    	// Setup for button controller
+    	bReload.setOnClickListener(new ImageButton.OnClickListener() {
+    	    @Override
+    	    public void onClick(View v) {
+    	    	webView.reload();
+    	    	enableControllerButton();
+    	    }
+    	});
+    	bBack.setOnClickListener(new ImageButton.OnClickListener() {
+    	    @Override
+    	    public void onClick(View v) {
+    	    	webView.goBack();
+    	    	enableControllerButton();
+    	    }
+    	});
+    	bForward.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.goForward();
+                enableControllerButton();
+            }
+        });
+
+        bDone.setOnClickListener(new ImageButton.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    	
+    	webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                Log.i("url : ", url);
+                if (url.contains("close/1")) {
+                    finish();
+                    return false;
+                } else {
+                    view.loadUrl(url);
+                }
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, android.graphics.Bitmap favicon) {
+
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                mIsLoadFinish = true;
+                enableControllerButton();
+                if (url.contains("https://m.facebook.com/v2.0/dialog/oauth")) {
+//                    Main.reloadFacebook = true;
+                }
+            }
+        });
+
+    	webView.setWebChromeClient(new WebChromeClient());
+    	
+    	webView.loadUrl(url);
+    }
+    
+	@Override
+ 	public void onBackPressed() {
+    	finish();
+    }
+	
+	public void enableControllerButton() {
+		
+		if (mIsLoadFinish) {
+			bReload.setEnabled(true);
+			if (webView.canGoBack()) {
+				bBack.setClickable(true);
+				bBack.setEnabled(true);
+                bBack.setAlpha(1f);
+		    } else {
+		    	bBack.setClickable(false);
+		    	bBack.setEnabled(false);
+                bBack.setAlpha(0.5f);
+		    }
+		    if (webView.canGoForward()) {
+		    	bForward.setClickable(true);
+		    	bForward.setEnabled(true);
+                bForward.setAlpha(1f);
+		    } else {
+		    	bForward.setClickable(false);
+		    	bForward.setEnabled(false);
+                bForward.setAlpha(0.5f);
+		    }
+		} else {
+			bBack.setClickable(false);
+		    bBack.setEnabled(false);
+		    bForward.setClickable(false);
+		    bForward.setEnabled(false);
+            bBack.setAlpha(0.5f);
+            bForward.setAlpha(0.5f);
+		}
+    }
+		
+}
--- var/apps/android/Siberian/app/src/main/java/com/siberiancms/angular/Main.java
+++ var/apps/android/Siberian/app/src/main/java/com/siberiancms/angular/Main.java
@@ -1,18 +1,177 @@
 package com.siberiancms.angular;
 
-import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.content.Context;
+import android.widget.Toast;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+import android.net.Uri;
+
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.GeolocationPermissions;
+
 
 
 public class Main extends Activity {
 
+   /**
+    * Main Webview
+    */
+    public static WebView webView;
+
+    public static Boolean webviewIsLoaded = false;
+    public static Boolean webviewHasFailed = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+
+        settings.setJavaScriptEnabled(true);
+        final ProgressDialog pd = ProgressDialog.show(Main.this, "",
+                this.getApplicationContext().getString(R.string.load_message), true);
+        webView.setInitialScale(1);
+        settings.setGeolocationEnabled(true);
+        settings.setUseWideViewPort(false);
+        settings.setJavaScriptCanOpenWindowsAutomatically(false);
+        settings.setSupportZoom(false);
+        settings.setSupportMultipleWindows(false);
+        settings.setBuiltInZoomControls(false);
+        settings.setDisplayZoomControls(false);
+
+        settings.setAppCachePath(getApplicationContext().getCacheDir().getAbsolutePath());
+        settings.setAllowFileAccess(true);
+        settings.setAppCacheEnabled(true);
+        settings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        checkConnection();
+
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+
+        webView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+
+                Main.webviewIsLoaded = !Main.webviewHasFailed;
+                pd.dismiss();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView Webview, String url) {
+                Log.i("url", url);
+
+                WebView.HitTestResult hr = Webview.getHitTestResult();
+                Log.i("Clicked", "getExtra = "+ hr.getExtra() + "\t\t Type=" + hr.getType());
+                Log.i("SRC_ANCHOR_TYPE", String.valueOf(WebView.HitTestResult.SRC_ANCHOR_TYPE));
+                Log.i("SRC_IMAGE_ANCHOR_TYPE", String.valueOf(WebView.HitTestResult.SRC_IMAGE_ANCHOR_TYPE));
+                if(hr.getType() == WebView.HitTestResult.SRC_ANCHOR_TYPE) {
+
+                    if (url.startsWith("tel:")) {
+                        startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(url)));
+                    } else if (url.startsWith("geo:")) {
+                        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+                    } else if (url.contains("www.youtube.com")) {
+                        Intent myWebLink = new Intent(Intent.ACTION_VIEW);
+                        Uri uri = Uri.parse(url);
+                        myWebLink.setDataAndType(uri, "text/html");
+                        myWebLink.addCategory(Intent.CATEGORY_BROWSABLE);
+                        startActivity(myWebLink);
+
+                    } else {
+                        Intent intent = new Intent(Main.this, Browser.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.putExtra("url", url);
+                        startActivity(intent);
+                    }
+
+                    return true;
+
+                }
+
+                return false;
+
+            }
+
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                view.setVisibility(WebView.GONE);
+                Toast.makeText(getApplicationContext(), R.string.no_internet_connection, Toast.LENGTH_LONG).show();
+                Main.webviewIsLoaded = false;
+                Main.webviewHasFailed = true;
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+            public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+                callback.invoke(origin, true, false);
+            }
+        });
+
+        Log.i("Loading URL", this.getApplicationContext().getString(R.string.url));
+        webView.loadUrl(this.getApplicationContext().getString(R.string.url));
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+
+            DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    if(which == DialogInterface.BUTTON_POSITIVE) {
+                        finish();
+                    }
+                }
+            };
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(webView.getContext());
+            builder.setMessage(R.string.quit_message).setPositiveButton(R.string.yes, dialogClickListener)
+                    .setNegativeButton(R.string.no, dialogClickListener).show();
+            return;
+
+        }
+    }
+
+    private void checkConnection() {
+
+        boolean isConnected = false;
+        ConnectivityManager check = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (check != null) {
+
+            NetworkInfo[] info = check.getAllNetworkInfo();
+            if (info != null) {
+                for (int i = 0; i < info.length; i++) {
+                    if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                        isConnected = true;
+                    }
+                }
+                if(!isConnected) {
+                    webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+                }
+            }
+
+        }
+
     }
 
 }
